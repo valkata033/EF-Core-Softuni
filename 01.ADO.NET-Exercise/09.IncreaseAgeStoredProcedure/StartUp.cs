8161709e91034df887fdd31d05ad55dd8186436b
Valentin <85406438+valkata033@users.noreply.github.com>
2022-09-02 19:14:31 +0300

    Add files via upload

---
+++ 01.ADO.NET-Exercise/09.IncreaseAgeStoredProcedure/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace _09.IncreaseAgeStoredProcedure
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int minionId = int.Parse(Console.ReadLine());
+
+            SqlConnection sqlConnection =
+                new SqlConnection(Connections.ConnectionString);
+            sqlConnection.Open();
+
+            string result = IncreaseMinionAge(sqlConnection, minionId);
+            Console.WriteLine(result);
+
+            sqlConnection.Close();
+
+        }
+
+        private static string IncreaseMinionAge(SqlConnection sqlConnection, int minionId)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string increaseAgeQuery = @"EXEC [dbo].[usp_GetOlder] @minionId";
+            SqlCommand increaseAgeCmd = new SqlCommand(increaseAgeQuery, sqlConnection);
+            increaseAgeCmd.Parameters.AddWithValue("@minionId", minionId);
+
+            increaseAgeCmd.ExecuteNonQuery();
+
+            string minionsInfoQuery = @"SELECT Name, 
+                                               Age 
+                                          FROM Minions 
+                                         WHERE Id = @minionId";
+
+            SqlCommand minionsInfoCmd = new SqlCommand(minionsInfoQuery, sqlConnection);
+            minionsInfoCmd.Parameters.AddWithValue("@minionId", minionId);
+
+            using SqlDataReader reader = minionsInfoCmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                sb.AppendLine($"{reader["Name"]} – {reader["Age"]} years old");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
